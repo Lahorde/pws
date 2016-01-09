317aaf6199b9024288823fa03af69d3fc82c6a81
remi <mr.lahorde@laposte.net>
2016-01-09 11:37:08 +0100

    Update template config file

--- pws_params.sh
+++ pws_params.sh
@@ -13,15 +13,19 @@ export WU_PWD=""
 # Weather station server prefix
 export VENT_PIOU_PIOU_URL_PREFIX="http://api.pioupiou.fr/v1/live/"
 
-# Selected WU stations for wind
+# WU stations for wind
 export VENT_1_URL_SUFFIX=""
-export VENT_2_URL_SUFFIX=""
+
+# Piou Piou stations for wind
 export VENT_1_PIOU_PIOU_URL_SUFFIX=""
 
-# Emoncms Key
+# Emoncms Key for home data
 export EMONCMS_KEY=""
 export EMONCMS_HUMIDITY_FIELD=""
 export EMONCMS_TEMP_FIELD=""
 
 # where are polling data
 export PWS_POLLING_DATA_PATH=""
+
+# logs
+export PWS_LOG_PATH=""
